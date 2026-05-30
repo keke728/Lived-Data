cad24833442b90567bc4365592ff3eab03b93121
Keke Wu <kekewu0728@gmail.com>
2026-05-30 00:00:41 -0400

    Update the home page

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 document.body.classList.remove("is-loading", "is-ready", "is-complete");
+
+const updateHeaderState = () => {
+  document.body.classList.toggle("is-scrolled", window.scrollY > 24);
+};
+
+updateHeaderState();
+window.addEventListener("scroll", updateHeaderState, { passive: true });
